fde24507721ef5126e56b8fd76a070734e9330b3
Sophie Haskins <sophiehaskins@navapbc.com>
2025-03-20 13:25:12 -0400

    chore: fix permissions for ingest task

--- terraform/modules/gost_consume_grants/compute.tf
+++ terraform/modules/gost_consume_grants/compute.tf
@@ -185,6 +185,7 @@ resource "aws_iam_role_policy" "execution" {
   for_each = {
     decrypt-datadog-api-key = module.decrypt_datadog_api_key_policy.json
     write-logs              = module.write_logs_policy.json
+    decrypt-github-creds    = module.decrypt_github_credentials_policy.json
   }
 
   name   = each.key
